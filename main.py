23237c348e3cb14b28a0e126176910b7fc052c9e
Titi <tristanfio05@gmail.com>
2025-02-27 09:30:33 +0100

    [DEV]: Filter graph

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import plotly.express as px
 st.set_page_config(layout='wide')
 
 title = "<center><h1>Ventes téléphone dashboard</h1></center>"
+st.markdown(title, unsafe_allow_html=True)
 
 dataframe = pd.read_excel('ventes_smartphones.xlsx')
 dataframe.columns = dataframe.columns.str.strip()
 dataframe = dataframe.drop(0)
 
-dataframe_groupby_retailer = dataframe['Retailer'].groupby(dataframe['Retailer']).sum().index
-dataframe_groupby_month = dataframe['InvoiceDate'].groupby(dataframe['InvoiceDate']).sum().index
+dataframe['InvoiceDate'] = pd.to_datetime(dataframe['InvoiceDate'], errors='coerce')
+
+dataframe_groupby_retailer = dataframe['Retailer'].unique()
+dataframe_groupby_month = dataframe['InvoiceDate'].dt.strftime('%Y-%m').unique()
 
 col1, col2, col3 = st.columns([0.1, 0.45, 0.45])
 col4, col5 = st.columns([0.50, 0.50])
@@ -19,44 +22,60 @@ col4, col5 = st.columns([0.50, 0.50])
 with col1:
     st.date_input('Date', value=pd.to_datetime('today', format='%d %m %Y'))
 
+with col4:
+    selected_retailer = st.selectbox(
+        '',
+        dataframe_groupby_retailer,
+        index=None,
+        placeholder='Select a retailer'
+    )
+
+    button_get_data_retailer = st.button('Get data', key='retailer')
+    
+    if button_get_data_retailer:
+        st.session_state['selected_retailer'] = selected_retailer
+
+with col5:
+    selected_month = st.selectbox(
+        '',
+        dataframe_groupby_month,
+        index=None,
+        placeholder='Select a month'
+    )
+
+    button_get_data_month = st.button('Get data', key='month')
+    
+    if button_get_data_month:
+        st.session_state['selected_month'] = selected_month
+
+if 'selected_retailer' in st.session_state and st.session_state['selected_retailer']:
+    dataframe_filtered = dataframe[dataframe['Retailer'] == st.session_state['selected_retailer']]
+else:
+    dataframe_filtered = dataframe
+
 with col2:
     fig_bar = px.bar(
-        dataframe,
-        x=dataframe['Retailer'],
-        y=dataframe['TotalSales'],
+        dataframe_filtered,
+        x='Retailer',
+        y='TotalSales',
         title='Total sales by retailer',
     )
     st.plotly_chart(fig_bar)
 
-    with col4:
-        option = st.selectbox(
-            '',
-            dataframe_groupby_retailer,
-            index=None,
-            placeholder='Select a retailer'
-        )
-
-        button_get_data_retailer = st.button('Get data', key='retailer')
+if 'selected_month' in st.session_state and st.session_state['selected_month']:
+    dataframe_filtered = dataframe[
+        dataframe['InvoiceDate'].dt.strftime('%Y-%m') == st.session_state['selected_month']
+    ]
 
 with col3:
     fig_line = px.line(
-        dataframe,
-        x=dataframe['InvoiceDate'],
-        y=dataframe['TotalSales'],
+        dataframe_filtered,
+        x='InvoiceDate',
+        y='TotalSales',
         title='Total sales Over Time',
     )
     st.plotly_chart(fig_line)
-    
-    with col5:
-        option = st.selectbox(
-            '',
-            dataframe_groupby_month,
-            index=None,
-            placeholder='Select a month'
-        )
 
-        button_get_data_month = st.button('Get data', key='month')
-        
 st.markdown(
     '<style>.stVerticalBlock.st-emotion-cache-1it2min.e6rk8up3 {display: block ruby}</style>',
     unsafe_allow_html=True
