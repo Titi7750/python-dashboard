71691c21d46f0c79608262a7aa1bcd536e671f5a
Titi <tristanfio05@gmail.com>
2025-02-27 10:29:14 +0100

    [DEV]: Graph with Bar & Line Chart

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(layout='wide')
 
 title = "<center><h1>Ventes téléphone dashboard</h1></center>"
-st.markdown(title, unsafe_allow_html=True)
 
 dataframe = pd.read_excel('ventes_smartphones.xlsx')
 dataframe.columns = dataframe.columns.str.strip()
@@ -54,13 +54,13 @@ else:
     dataframe_filtered = dataframe
 
 with col2:
-    fig_bar = px.bar(
+    fig_bar_retailer = px.bar(
         dataframe_filtered,
         x='Retailer',
         y='TotalSales',
         title='Total sales by retailer',
     )
-    st.plotly_chart(fig_bar)
+    st.plotly_chart(fig_bar_retailer)
 
 if 'selected_month' in st.session_state and st.session_state['selected_month']:
     dataframe_filtered = dataframe[
@@ -68,15 +68,54 @@ if 'selected_month' in st.session_state and st.session_state['selected_month']:
     ]
 
 with col3:
-    fig_line = px.line(
+    fig_line_month = px.line(
         dataframe_filtered,
         x='InvoiceDate',
         y='TotalSales',
         title='Total sales Over Time',
     )
-    st.plotly_chart(fig_line)
+    st.plotly_chart(fig_line_month)
 
 st.markdown(
     '<style>.stVerticalBlock.st-emotion-cache-1it2min.e6rk8up3 {display: block ruby}</style>',
     unsafe_allow_html=True
 )
+
+# Total Sales and Units Sold by State with Bar and Line Chart & Graph Objects
+fig_bar_line = go.Figure()
+
+state_groupby = dataframe.groupby('State').agg({
+    'TotalSales': 'sum',
+    'UnitsSold': 'sum'
+}).reset_index()
+
+fig_bar_line.add_trace(
+    go.Bar(
+        x=state_groupby['State'],
+        y=state_groupby['TotalSales'],
+        name='Total Sales',
+        marker_color='blue'
+    )
+)
+
+fig_bar_line.add_trace(
+    go.Scatter(
+        x=state_groupby['State'],
+        y=state_groupby['UnitsSold'],
+        yaxis='y2',
+        name='Units Sold',
+        mode='lines',
+        line=dict(color='orange')
+    )
+)
+
+fig_bar_line.update_layout(
+    title='Total Sales and Units Sold by State',
+    xaxis_title='State',
+    yaxis=dict(title="Total Sales", side="left"),
+    yaxis2=dict(title="Units Sold", overlaying="y", side="right"),
+    legend=dict(x=1, y=1),
+    barmode='group'
+)
+
+st.plotly_chart(fig_bar_line)
